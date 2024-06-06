61d4d37532880e06795f268d100821347c4166d1
Mattia Dei Rossi <885768@stud.unive.it>
2024-06-06 19:24:51 +0200

    comment

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -4,7 +4,7 @@ int main()
 {
    mongo mongo("mongodb://user:pwd@10.5.0.5:27017/vpndb");
    mongo.add_user("0", "user1", "psw");
-   mongo.get_users();
+   mongo.get_users(); // print users
 
    auto hash = mongo.hash_password("psw");
    std::cout<< mongo.is_present("user1", hash) << std::endl;

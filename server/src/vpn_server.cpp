48fe2fa0b666132d8f1a56dadecd614107918842
El Rubio <879899@stud.unive.it>
2024-07-30 22:24:12 +0200

    minor

--- server/src/vpn_server.cpp
+++ server/src/vpn_server.cpp
@@ -10,315 +10,11 @@
 #include "vpn_data_utils.h"
 #include "tun_utils.h"
 #include "holder.h"
+#include "selector.h"
 #include "mongo.hpp"
-
-socket_utils::socket_t extract_socket(socket_holder *holder) {
-
-    socket_type type = holder->type;
-    switch (type) {
-    case TCP_CLIENT_SOCKET:
-        return (holder->data).tcs->socket;
-    case TCP_SERVER_SOCKET:
-        return (holder->data).tss->socket;
-    case UDP_SERVER_SOCKET:
-        return (holder->data).uss->socket;
-    default:
-        exit(1);
-    }
-
-    return -1;
-}
-
-void tcs_free(tcp_client_socket *tcs) {
-
-    // Sanity check.
-    if (tcs == NULL) return;
-
-    ssl_utils::free_ssl(tcs->ssl, NULL);
-    free(tcs);
-}
-
-int create_tss(
-    char const *host, 
-    char const *port,
-    tcp_server_socket **tcp_socket
-) {
-
-    int ret_val = 0;
-    
-    socket_utils::socket_t socket;
-    if (socket_utils::bind_tcp_server_socket(host, port, &socket) == -1) {
-        utils::print_error("create_tss: cannot create TCP server socket\n");
-        return -1;
-    }
-
-    tcp_server_socket *ret_data = (tcp_server_socket *) malloc(sizeof(tcp_server_socket));
-    if (!ret_data) {
-        utils::print_error("create_tss: cannot create TCP server socket data\n");
-        socket_utils::close_socket(socket);
-        return -1;
-    }
-
-    // Setting up the tcp server socket.
-    socket_utils::log_start_server(true, host, port);
-    ret_data->socket = socket;
-    *tcp_socket = ret_data;
-
-    return ret_val;
-}
-
-void tss_free(tcp_server_socket *tss) {
-
-    // Sanity check.
-    if (tss == NULL) return;
-
-    socket_utils::close_socket(tss->socket);
-    free(tss);
-}
-
-int create_uss(char const *host, char const *port, udp_server_socket **udp_socket) {
-
-    /* A UDP socket does not need to set itself to a listen state.
-    *  Just up to bind. 
-    */
-    socket_utils::socket_t socket;
-    if (socket_utils::bind_udp_server_socket(host, port, &socket) == -1) {
-        utils::print_error("create_uss: cannot create UDP server socket\n");
-        return -1;
-    }
-
-    udp_server_socket *uss = (udp_server_socket *) malloc(sizeof(udp_server_socket));
-    if (!uss) {
-        utils::print_error("create_uss: cannot create UDP server socket data\n");
-        socket_utils::close_socket(socket);
-        return -1;
-    }
-
-    // Setting up the udp server socket.
-    socket_utils::log_start_server(false, host, port);
-    uss->socket = socket;
-    *udp_socket = uss;
-
-    return 0;
-}
-
-void uss_free(udp_server_socket *udp_socket) {
-
-    // Sanity check.
-    if (udp_socket == NULL) return;
-
-    socket_utils::close_socket(udp_socket->socket);
-    free(udp_socket);
-}
-
-void socket_data_free(socket_type type, socket_data data) {
-
-    switch (type) {
-    case TCP_CLIENT_SOCKET:
-        tcs_free(data.tcs);
-        break;
-    case TCP_SERVER_SOCKET:
-        tss_free(data.tss);
-        break;
-    case UDP_SERVER_SOCKET:
-        uss_free(data.uss);
-        break;
-    default:
-        exit(1);
-    }
-}
-
-int create_sh(socket_type type, socket_data data, socket_holder **sh) {
-
-    int ret_val = 0;
-
-    socket_holder *holder = (socket_holder *) malloc(sizeof(socket_holder));
-    if (!holder) {
-        socket_data_free(type, data);
-        return -1;
-    }
-
-    holder->data = data;
-    holder->type = type;
-    *sh = holder;
-
-    return ret_val;
-}
-
-void socket_holder_free(socket_holder *sh) {
-
-    // Sanity check.
-    if (sh == NULL) return;
-
-    socket_data_free(sh->type, sh->data);
-    free(sh);
-}
-
-int create_tss_sh(
-    char const *host, 
-    char const *port,
-    socket_holder **sh
-) {
-
-    int ret_val = 0;
-
-    // Creating tcp server socket.
-    tcp_server_socket *tss;
-    ret_val = create_tss(host, port, &tss);
-    if (ret_val) return ret_val;
-
-    socket_holder *holder;
-    socket_data data;
-    data.tss = tss;
-    ret_val = create_sh(TCP_SERVER_SOCKET, data, &holder);
-    if (ret_val) return ret_val;
-
-    *sh = holder;
-
-    return ret_val;
-}
-
-int create_uss_sh(
-    char const *host, 
-    char const *port, 
-    socket_holder **sh
-) {
-
-    int ret_val = 0;
-
-    // Creating udp server socket.
-    udp_server_socket *uss;
-    ret_val = create_uss(host, port, &uss);
-    if (ret_val) return ret_val;
-
-    socket_holder *holder;
-    socket_data data;
-    data.uss = uss;
-    ret_val = create_sh(UDP_SERVER_SOCKET, data, &holder);
-    if (ret_val) return ret_val;
-
-    *sh = holder;
-
-    return ret_val;
-}
-
-void map_set_max_add(
-    std::map<socket_utils::socket_t, socket_holder*>& map, 
-    fd_set *set,
-    socket_holder *holder,
-    socket_utils::socket_t *max_socket
-) {
-
-    // Assuming extract_socket does not retun -1.
-    socket_utils::socket_t socket = extract_socket(holder);
-
-    // Updating the max_socket variable.
-    if (socket > *max_socket) {
-        *max_socket = socket;
-    }
-
-    // Adding a new socket to handle to the master set.
-    FD_SET(socket, set);
-
-    // Keeping track of the holder for further usage.
-    map[socket] = holder;
-}
 
 void ssl_context_free(SSL_CTX *ctx) {
     if (ctx != NULL) SSL_CTX_free(ctx);
-}
-
-void map_set_max_free(
-    std::map<socket_utils::socket_t, socket_holder*>& map, 
-    fd_set *master_set,
-    socket_utils::socket_t *max_socket
-) {
-
-    for (auto iter = map.begin(); iter != map.end(); ++iter) {
-
-        // Getting key and value.
-        socket_utils::socket_t socket = iter->first;
-        socket_holder *holder = iter->second;
-
-        // Removing socket from the master set and freeing the holder.
-        FD_CLR(socket, master_set);
-        socket_holder_free(holder);
-    }
-
-    /* Clearing the map.
-    *  Zeroing the max_socket.
-    */
-    map.clear();
-    *max_socket = 0;
-}
-
-// TODO Assumptions ...
-int map_set_max_delete_client(
-    socket_holder *holder,
-    std::map<socket_utils::socket_t, socket_holder*>& map, 
-    fd_set *master_set,
-    socket_utils::socket_t *max_socket
-) {
-
-    int ret_val = 0;
-    tcp_client_socket *tcs = holder->data.tcs;
-    socket_utils::socket_t socket = tcs->socket;
-
-    // Updating the max_socket value.
-    if (socket == *max_socket) {
-
-        // This is possible since the because the map in C++ is in increasing order of keys by default.
-        auto pair = ++map.rbegin();
-        socket_utils::socket_t max_socket_from_map = pair->first;
-        *max_socket = max_socket_from_map;
-    }
-
-    /* Removing socket from the master set.
-    *  Freeing the holder.
-    *  Deleting freed holder reference from the map.
-    */
-    FD_CLR(socket, master_set);
-    socket_holder_free(holder);
-    map.erase(socket);
-
-    return ret_val;
-}
-
-int map_check_socket(socket_holder *holder, socket_type type) {
-    if (holder == NULL) return 0;
-    if (holder->type == type) return 1;
-    return 0;
-}
-
-int map_is_tss(socket_holder *holder) {
-    return map_check_socket(holder, TCP_SERVER_SOCKET);
-}
-
-int map_is_uss(socket_holder *holder) {
-    return map_check_socket(holder, UDP_SERVER_SOCKET);
-}
-
-int map_is_tcs(socket_holder *holder) {
-    return map_check_socket(holder, TCP_CLIENT_SOCKET);
-}
-
-void uc_map_update(
-    user_id id,
-    std::map<int, udp_client_info_utils::udp_client_info>& map,
-    std::shared_mutex& mutex,
-    udp_client_info_utils::udp_client_info *new_info
-) {
-
-    /* Updating the map.
-    *  Mutex is needed to protect shared memory.
-    */
-    std::unique_lock lock(mutex);
-
-    if (map.count(id)) map.erase(id);
-
-    if (new_info) {
-        map[id] = *new_info;
-    }
 }
 
 void handle_tcp_client_key_exchange_2(
@@ -345,9 +41,7 @@ void handle_tcp_client_key_exchange(
     socket_utils::socket_t client_socket,
     struct sockaddr_storage client_address,
     socklen_t client_len,
-    SSL_CTX *ctx,
-    std::map<int, udp_client_info_utils::udp_client_info>& map, 
-    std::shared_mutex& mutex
+    SSL_CTX *ctx
 ) {
 
     SSL *ssl;
@@ -411,11 +105,8 @@ void handle_tcp_client_key_exchange(
         return;
     }
 
-    uc_map_update(db_user_id, map, mutex, &info);
-
     if (ssl_utils::write(ssl, message, message_length) == -1) {
         utils::print_error("handle_tcp_client_key_exchange: key cannot be shared with the client\n");
-        uc_map_update(db_user_id, map, mutex, NULL);
         return;
     }
 
@@ -425,11 +116,6 @@ void handle_tcp_client_key_exchange(
     *   - Release UDP resources before the TCP connection goes away
     */
     ssl_utils::free_ssl(ssl, NULL);
-}
-
-void map_uc_free(std::map<int, udp_client_info_utils::udp_client_info>& map, std::shared_mutex& mutex) {
-    std::unique_lock lock(mutex);
-    map.clear();
 }
 
 /* Functions that deals with shared data must always return new data:
@@ -472,8 +158,7 @@ int extract_vpn_client_packet_data(
 */
 int handle_incoming_udp_packet(
     socket_utils::socket_t udp_socket, 
-    std::map<int, udp_client_info_utils::udp_client_info>& map, 
-    std::shared_mutex& mutex, 
+    holder::client_register *c_register,
     encryption::packet *ret_packet
 ) {
 
@@ -481,7 +166,7 @@ int handle_incoming_udp_packet(
     socklen_t client_len = sizeof(client_address);
 
     /* Using the theoretical limit of an UDP packet.
-    *  Instead of setting the MSG_PEEK flag, on safe bet is made on how much data to allocate.
+    *  Instead of setting the MSG_PEEK flag, a safe bet is made on how much data to allocate.
     */
     encryption::packet pkt;
     memset(&pkt, 0, sizeof(encryption::packet));
@@ -490,6 +175,8 @@ int handle_incoming_udp_packet(
         sizeof(pkt.message), 0,
         (struct sockaddr *) &client_address, &client_len
     );
+
+    // TODO: Try delete with address.
 
     /* The value of zero is not considered an error.
     *  A connection can be closed by the other peer.
@@ -527,8 +214,8 @@ int handle_incoming_udp_packet(
     sscanf((const char *) vpn_data.user_id, "%d", &id_num);
     user_id user_id = id_num;
 
-    char key[encryption::MAX_KEY_SIZE];
-    if (map_uc_extract_key(user_id, map, mutex, key) == -1) return -1;
+    unsigned char key[holder::SIZE_32];
+    if (holder::extract_client_key(c_register, user_id, key) == -1) return -1;
 
     encryption::encryption_data enc_data;
     memcpy(enc_data.key, key, encryption::MAX_KEY_SIZE);
@@ -548,54 +235,39 @@ int handle_incoming_udp_packet(
 
 int start_doge_vpn() {
 
-    int ret_val = 0;
-    socket_utils::socket_t max_socket = 0;
-
     SSL_CTX *ctx = NULL;
-    socket_holder *tss_holder = NULL;
-    socket_holder *uss_holder = NULL;
-
-    std::map<socket_utils::socket_t, socket_holder*> sh_map;
-    std::map<user_id, udp_client_info_utils::udp_client_info> uc_map;
-    std::shared_mutex uc_map_mutex;
+    holder::client_register c_register;
+    selector::selector_set s_set = selector::empty_set();
 
     tun_utils::ip_pool_t pool;
     tun_utils::configure_private_class_c_pool(11, &pool);
 
-    holder::client_register c_register;
+    if (ssl_utils::init_ssl(&ctx, true, "certs/cert.pem", "certs/key.pem") == -1) goto error_handler;
 
-    fd_set master;
+    holder::socket_holder server_tcp_holder;
+    if (holder::init_tcp_server_holder("0.0.0.0", "8080", &server_tcp_holder) == -1) goto error_handler;
 
-    // SSL initialization.
-    ret_val = ssl_utils::init_ssl(&ctx, true, "certs/cert.pem", "certs/key.pem");
-    if (ret_val) goto error_handler;
+    holder::socket_holder server_udp_holder;
+    if (holder::init_udp_server_holder("0.0.0.0", "8080", &server_udp_holder) == -1) goto error_handler;
 
-    // Initialization of master set for further selects.
-    FD_ZERO(&master);
-
-    // Initialization of the tcp server socket.
-    ret_val = create_tss_sh("0.0.0.0", "8080", &tss_holder);
-    if (ret_val) goto error_handler;
-    map_set_max_add(sh_map, &master, tss_holder, &max_socket);
-
-    // Initialization of the udp server socket.
-    ret_val = create_uss_sh("0.0.0.0", "8080", &uss_holder);
-    if (ret_val) goto error_handler;
-    map_set_max_add(sh_map, &master, uss_holder, &max_socket);
+    /* Once server sockets have been created:
+    *   - the fd_set must be updated
+    *   - the max_socket must be updated
+    */
+    selector::add(&s_set, holder::extract_socket(&server_tcp_holder));
+    selector::add(&s_set, holder::extract_socket(&server_udp_holder));
 
     while(true) {
 
         // Copy of master, otherwise we would lose its data.
-        fd_set reads;
-        reads = master;
+        fd_set reads = s_set.socket_set;
+        if (selector::wait_select(&s_set, &reads) < 0) goto error_handler;
 
-        if (select(max_socket + 1, &reads, 0, 0, 0) < 0) goto error_handler;
+        for (socket_utils::socket_t socket = 0; socket <= s_set.max_socket; ++socket) {
 
-        for (socket_utils::socket_t socket = 0; socket <= max_socket; ++socket) {
+           if (selector::is_set(&reads, socket)) {
 
-           if (FD_ISSET(socket, &reads)) {
-
-                if (socket == extract_socket(tss_holder)) {
+                if (socket == holder::extract_socket(&server_tcp_holder)) {
 
                     /* The TCP socket is ready to accept a new client.
                     *  By calling accept, a new client socket will be created.
@@ -630,10 +302,10 @@ int start_doge_vpn() {
 
                         th.detach();
                     }
-                } else if (socket == extract_socket(uss_holder)) {
+                } else if (socket == holder::extract_socket(&server_udp_holder)) {
 
                     encryption::packet received_packet;
-                    if (handle_incoming_udp_packet(socket, uc_map, uc_map_mutex, &received_packet) == -1) {
+                    if (handle_incoming_udp_packet(socket, &c_register, &received_packet) == -1) {
                         utils::print_error("start_doge_vpn: udp packet of client cannot be verified\n");
                     } else {
 
@@ -659,8 +331,8 @@ error_handler:
 
     // Cleaning up resources.
     ssl_context_free(ctx);
-    map_set_max_free(sh_map, &master, &max_socket);
-    map_uc_free(uc_map, uc_map_mutex);
+    // map_set_max_free(sh_map, &master, &max_socket);
+    // map_uc_free(uc_map, uc_map_mutex);
 
     return -1;
 }

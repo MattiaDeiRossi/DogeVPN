aaa31895daecc1232e4a50302f8dc0b052b8e687
El Rubio <879899@stud.unive.it>
2024-08-14 13:33:42 +0200

    add autoclose

--- build_run.sh
+++ build_run.sh
@@ -6,6 +6,18 @@ docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 sudo docker network prune -f
 
+# Close all the opened terminals
+
+if test -f opened_bashpids.txt; then
+
+    cat opened_bashpids.txt | while read line 
+    do
+        kill -9 "$line"
+    done
+
+    rm "opened_bashpids.txt"
+fi
+
 # Start containers again
 
 sudo docker compose build
@@ -14,13 +26,18 @@ sudo docker compose up -d
 
 # Attach terminal's standard input, output, and error
 # Need to install dbus-x11 for running gnome-terminal command easily
+# Create file for saving pids
+
+if ! test -f opened_bashpids.txt; then
+  touch opened_bashpids.txt
+fi
 
 CONTAINER_NAMES=$(sudo docker ps --format "{{.Names}}")
 
 for cn in $CONTAINER_NAMES
 do
     echo "$cn" > "holder.txt"
-    gnome-terminal -- sh -c 'sudo docker attach $(head -n 1 holder.txt); $SHELL'
+    gnome-terminal -- sh -c 'sudo docker attach $(head -n 1 holder.txt); echo $$ >> opened_bashpids.txt; $SHELL'
 done
 
 rm "holder.txt"
--- erase_cts.sh
+++ erase_cts.sh
@@ -3,3 +3,15 @@
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 sudo docker network prune -f
+
+# Close all the opened terminals
+
+if test -f opened_bashpids.txt; then
+
+    cat opened_bashpids.txt | while read line 
+    do
+        kill -9 "$line"
+    done
+
+    rm "opened_bashpids.txt"
+fi

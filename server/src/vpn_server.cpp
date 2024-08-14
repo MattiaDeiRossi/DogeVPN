d0c34725b327ed70743e3ae12c5a804cdef38b7b
El Rubio <879899@stud.unive.it>
2024-08-14 11:05:40 +0200

    minor

--- server/src/vpn_server.cpp
+++ server/src/vpn_server.cpp
@@ -206,6 +206,14 @@ void start_doge_vpn() {
 }
 
 int main() {
+
+    /*encryption::packet pkt;
+    encryption::encryption_data ed((const unsigned char *)"key");
+
+    pkt.append((const unsigned char *) "hello", strlen("hello"));
+    encryption::packet e_pkt = pkt.encrypt(ed).value();
+    printf("??: %s", e_pkt.decrypt(ed).value().buffer);*/
+
 	start_doge_vpn();
     return 0;
 }

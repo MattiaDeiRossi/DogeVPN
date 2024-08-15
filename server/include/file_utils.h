f062bed2f68b1174c4bc6866f2ab908d2a8b5696
El Rubio <879899@stud.unive.it>
2024-08-15 17:52:14 +0200

    add find_in_multi_key_value_lines

--- server/include/file_utils.h
+++ server/include/file_utils.h
@@ -2,14 +2,20 @@
 #define FILE_UTILS_H
 
 #include <map>
+#include <vector>
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <iterator>
+#include <optional>
 
 namespace file_utils {
 
-    std::map<std::string, std::string> parse_key_value_lines(const char *path, char delimiter);
+    std::map<std::string, std::string> parse_key_value_lines(const char *path);
+
+    std::vector<std::map<std::string, std::string>> parse_multi_key_value_lines(const char *path);
+
+    std::optional<std::map<std::string, std::string>> find_in_multi_key_value_lines(const char *path, const char *key, const char *value);
 }
 
 #endif

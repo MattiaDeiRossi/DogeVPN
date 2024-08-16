f30d5a4e80674869f38110d420228303a6bc71c1
El Rubio <879899@stud.unive.it>
2024-08-16 10:09:07 +0200

    remove private func from h file

--- lib/encryption.h
+++ lib/encryption.h
@@ -53,12 +53,6 @@ namespace encryption
 
         void log();
     };
-
-    int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-                unsigned char *iv, unsigned char *ciphertext);
-
-    int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-                unsigned char *iv, unsigned char *plaintext);
 }
 
 #endif

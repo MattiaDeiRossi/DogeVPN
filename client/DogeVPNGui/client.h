fc6ead7abdb4e55448e01372c7bfa916bd286116
El Rubio <879899@stud.unive.it>
2024-08-18 11:10:36 +0200

    add comment for client.h

--- client/DogeVPNGui/client.h
+++ client/DogeVPNGui/client.h
@@ -1,10 +1,22 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-/* Global variables */
+/* Global variables stop_flag. This won't be touched by the client program, but it is made
+ * available for stopping the VPN loop and disconnect from the server.
+ */
 extern bool stop_flag;
+
+/* For setting the stop flag: this is not thread-safe */
 void set_stop_flag(bool status);
 
+/* Start the loop for exchange data between the client and the server.
+ * @param domain domain name of the server to connect with, a valid name or a valid IPv4 address
+ * @param port port at which the server will listen for TCP and UDP packets
+ * @param user username that can authenticate to the server
+ * @param pwd password of the user
+ * @param device_name name to assign to the TUN interface that will be created after the key exchange
+ * @param network private network to reach
+ */
 int start_doge_vpn(
     char const *domain,
     char const *port,

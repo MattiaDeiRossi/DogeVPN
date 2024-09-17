944cb0f77884ac264cb05f5a80496484cee35628
El Rubio <879899@stud.unive.it>
2024-09-17 11:07:04 +0200

    add session_poller for handling sessions

---
+++ lib/session_poller.cpp
@@ -0,0 +1,44 @@
+#include "session_poller.h"
+
+namespace session_poller
+{
+
+    pool_t::pool_t(unsigned int max)
+    {
+        for (unsigned int i = 0; i < max; ++i)
+        {
+            session_pool.insert(i);
+        }
+    }
+
+    std::optional<unsigned int> pool_t::pop_next()
+    {
+        std::unique_lock lock(mutex);
+
+        if (session_pool.empty())
+        {
+            return std::nullopt;
+        }
+
+        unsigned int session_id = *session_pool.begin();
+        session_pool.erase(session_pool.begin());
+
+        return session_id;
+    }
+
+    void pool_t::push_back(unsigned int session)
+    {
+        std::unique_lock lock(mutex);
+
+        if (session_pool.count(session) != 0)
+        {
+            /* Since the session must be handled carefully,
+            /* it is the caller's job to ensure that there are no duplicates in the application.
+            */
+            throw std::invalid_argument("The given session is already present");
+        }
+
+        session_pool.insert(session);
+    }
+
+}
---
+++ lib/session_poller.h
@@ -0,0 +1,26 @@
+#ifndef SESSION_POLLER_H
+#define SESSION_POLLER_H
+
+#include <set>
+#include <optional>
+#include <shared_mutex>
+#include <mutex>
+
+namespace session_poller
+{
+
+    struct pool_t {
+
+        std::shared_mutex mutex;
+        std::set<unsigned int> session_pool;
+
+        pool_t(unsigned int max);
+
+        std::optional<unsigned int> pop_next();
+
+        void push_back(unsigned int session);
+    };
+}
+
+
+#endif

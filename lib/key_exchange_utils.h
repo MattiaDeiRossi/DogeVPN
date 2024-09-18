b4c7ab0c02578f25215582a7c9f7743ac5c6c640
El Rubio <879899@stud.unive.it>
2024-09-18 09:39:41 +0200

    add documentation

--- lib/key_exchange_utils.h
+++ lib/key_exchange_utils.h
@@ -8,17 +8,27 @@
 
 #include <socket_utils.h>
 
-/* The authentication server sends the client a message consisting of:
- *  - a session identifier (Ids)
- *  - a pseudorandom string (A)
- * The client receives the message from the server and sends a response consisting of:
- *  - username
- *  - a pseudorandom string (B)
- *  - the pseudorandom string (A), the session identifier (IdS), the user password, under the SHA256.
- * The server receives the message from the client, checks it and sends the relevant response consisting of:
- *  - the outcome of the connection attempt
- *  - the pseudorandom string (B) and the user's password are all encrypted
- * The client receives the response and uses the session if authentication has occurred, otherwise it interrupts the connection
+/**
+ * The authentication process involves several key steps between the server and the client:
+ *
+ * 1. Server to Client:
+ *    - The authentication server sends a message containing:
+ *      - A pseudorandom string (A)
+ *
+ * 2. Client Response:
+ *    - The client receives the message and constructs a response that includes:
+ *      - The username
+ *      - A new pseudorandom string (B)
+ *      - A hashed combination of:
+ *        - The pseudorandom string (A)
+ *        - The user's secret (hashed using SHA256)
+ *
+ * 3. Server Verification:
+ *    - Upon receiving the client's message, the server checks its validity and sends a response containing:
+ *      - Both the pseudorandom string (B) and the user's secret, which are hashed using SHA256
+ *
+ * 4. Client Action:
+ *    - The client processes the server's response. If authentication is successful, it will use the session; otherwise, it will terminate the connection.
  */
 namespace key_exchange_utils
 {
@@ -32,7 +42,10 @@ namespace key_exchange_utils
 
     struct credential_fetcher
     {
-        /**/
+        /**
+         * This pure virtual method must be implemented by any derived class.
+         * It is responsible for fetching the secret associated with a specific username.
+         */
         virtual std::string secret_by_username(std::string username) = 0;
     };
 
@@ -43,10 +56,18 @@ namespace key_exchange_utils
         m2_server
     };
 
+    /**
+     * @struct altered_MS_CHAPV2_identifier_t
+     * @brief Represents an identifier for the altered MS-CHAPv2 protocol.
+     *
+     * This struct encapsulates information related to an altered MS-CHAPv2 message.
+     * It includes the message type and a protocol name. The struct provides
+     * constructors for initialization and a method to convert the object to a
+     * string representation.
+     */
     struct altered_MS_CHAPV2_identifier_t
     {
         altered_MS_CHAPV2_message_type message_type;
-
         char protocol_name[MAX_PROTOCOL_NAME_SIZE];
 
         altered_MS_CHAPV2_identifier_t();
@@ -55,13 +76,17 @@ namespace key_exchange_utils
         std::string to_s();
     };
 
-    /**/
+    /**
+     * @struct altered_MS_CHAPV2_m1_server_sender_t
+     * @brief Represents the sender for the altered MS-CHAPv2 M1 server message.
+     *
+     * This struct is responsible for holding the message identifier and the server challenge
+     * used in the altered MS-CHAPv2 authentication protocol. It provides a constructor for
+     * initialization and a method to send the message over an SSL connection.
+     */
     struct altered_MS_CHAPV2_m1_server_sender_t
     {
-
-        /**/
         altered_MS_CHAPV2_identifier_t message_identifier;
-
         unsigned char server_challenge[MAX_CHALLENGE_SIZE];
 
         altered_MS_CHAPV2_m1_server_sender_t();
@@ -69,13 +94,17 @@ namespace key_exchange_utils
         void send(SSL *ssl);
     };
 
-    /**/
+    /**
+     * @struct altered_MS_CHAPV2_m1_server_receiver_t
+     * @brief Represents the receiver for the altered MS-CHAPv2 M1 server message.
+     *
+     * This struct is designed to handle the reception of the M1 server message in the altered
+     * MS-CHAPv2 authentication protocol. It contains the message identifier and the server
+     * challenge, as well as methods for initialization and message reception.
+     */
     struct altered_MS_CHAPV2_m1_server_receiver_t
     {
-
-        /**/
         altered_MS_CHAPV2_identifier_t message_identifier;
-
         unsigned char server_challenge[MAX_CHALLENGE_SIZE];
 
         altered_MS_CHAPV2_m1_server_receiver_t();
@@ -83,14 +112,18 @@ namespace key_exchange_utils
         void receive(SSL *ssl);
     };
 
-    /**/
+    /**
+     * @struct altered_MS_CHAPV2_m1_client_sender_t
+     * @brief Represents the sender for the altered MS-CHAPv2 M1 client message.
+     *
+     * This struct is responsible for preparing and sending the M1 client message in the
+     * altered MS-CHAPv2 authentication protocol. It contains the message identifier,
+     * user credentials, and challenges required for the authentication process.
+     */
     struct altered_MS_CHAPV2_m1_client_sender_t
     {
-        /**/
         altered_MS_CHAPV2_identifier_t message_identifier;
-
         char username[MAX_USERNAME_SIZE];
-
         unsigned char client_challenge[MAX_CHALLENGE_SIZE];
         unsigned char server_challenge[MAX_CHALLENGE_SIZE];
         unsigned char shared_secret[MAX_KEY_SIZE];
@@ -103,14 +136,18 @@ namespace key_exchange_utils
         void send(SSL *ssl);
     };
 
-    /**/
+    /**
+     * @struct altered_MS_CHAPV2_m1_client_receiver_t
+     * @brief Represents the receiver for the altered MS-CHAPv2 M1 client message.
+     *
+     * This struct handles the reception of the M1 client message from the client, storing relevant
+     * details such as the username and challenges. It provides methods for receiving the message,
+     * and validating the client's response.
+     */
     struct altered_MS_CHAPV2_m1_client_receiver_t
     {
-        /**/
         altered_MS_CHAPV2_identifier_t message_identifier;
-
         char username[MAX_USERNAME_SIZE];
-
         unsigned char client_challenge[MAX_CHALLENGE_SIZE];
         unsigned char hashed_challenge[MAX_HASH_SIZE];
 
@@ -123,13 +160,19 @@ namespace key_exchange_utils
             unsigned const char *shared_secret);
     };
 
+    /**
+     * @struct altered_MS_CHAPV2_m2_server_sender_t
+     * @brief Represents the sender for the altered MS-CHAPv2 M2 server message.
+     *
+     * This struct is responsible for preparing and sending the M2 server message in the
+     * altered MS-CHAPv2 authentication protocol. It contains the client challenge,
+     * shared secret, and a message identifier, allowing the server to respond
+     * appropriately to the client's previous request.
+     */
     struct altered_MS_CHAPV2_m2_server_sender_t
     {
-
-        /**/
         unsigned char client_challenge[MAX_CHALLENGE_SIZE];
         unsigned char shared_secret[MAX_KEY_SIZE];
-
         altered_MS_CHAPV2_identifier_t message_identifier;
 
         altered_MS_CHAPV2_m2_server_sender_t(unsigned const char *client_challenge, unsigned const char *shared_secret);
@@ -137,11 +180,19 @@ namespace key_exchange_utils
         void send(SSL *ssl);
     };
 
+    /**
+     * @struct altered_MS_CHAPV2_m2_server_receiver_t
+     * @brief Represents the receiver for the altered MS-CHAPv2 M2 server message.
+     *
+     * This struct handles the reception of the M2 message from the server in the
+     * altered MS-CHAPv2 authentication protocol. It stores the message identifier
+     * and the hashed challenge, and provides methods to receive the message and
+     * validate the server's response.
+     */
     struct altered_MS_CHAPV2_m2_server_receiver_t
     {
         altered_MS_CHAPV2_identifier_t message_identifier;
         unsigned char hashed_challenge[MAX_HASH_SIZE];
-
         altered_MS_CHAPV2_m2_server_receiver_t();
 
         void receive(SSL *ssl);
